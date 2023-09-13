193bebaf509f39582c71e740dd032e5b45a4b660
Basel Diab <114107867+Baseldiab@users.noreply.github.com>
2023-09-13 05:50:48 +0300

    'second'

--- js/main.js
+++ js/main.js
@@ -2,6 +2,10 @@ const passwordVol = document.querySelector(".password__vol");
 const inputVol = document.querySelector(".password__input-vol");
 const randomPassword = document.querySelector(".password-random");
 const generateButton = document.querySelector(".generate-btn");
+const copyToClipboard = document.querySelector(".btn-copy");
+const formCheckBox = document.querySelector(".form-checkbox");
+
+const checkedBoxes = document.querySelectorAll('[type="checkbox"]');
 
 // ========================
 inputVol.addEventListener("change", function () {
@@ -9,12 +13,58 @@ inputVol.addEventListener("change", function () {
   console.log(inputVol.value);
 });
 // ========================
+copyToClipboard.addEventListener("click", copyTextToClipboard);
+// ========================
+function copyTextToClipboard() {
+  navigator.clipboard.writeText(randomPassword.textContent);
+}
+// ========================
 generateButton.addEventListener("click", generatePassword);
 // ========================
 function generatePassword() {
-  let charset =
+  const uppercaseCheckBox = formCheckBox.uppercase.checked;
+  const lowercaseCheckBox = formCheckBox.lowercase.checked;
+  const numberCheckBox = formCheckBox.number.checked;
+  const symbolCheckBox = formCheckBox.symbol.checked;
+  let charset;
+  charset =
     "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()_+~`|}{[]:;?><,./-=";
-  // let charset = /\w/g;
+  passwordCases(
+    charset,
+    uppercaseCheckBox,
+    lowercaseCheckBox,
+    numberCheckBox.symbolCheckBox
+  );
+}
+// ========================
+function passwordCases(
+  charset,
+  uppercaseCheckBox,
+  lowercaseCheckBox,
+  numberCheckBox,
+  symbolCheckBox
+) {
+  let charPool = "";
+  if (uppercaseCheckBox) {
+    charPool += charset.match(/[A-Z]+/g);
+    getPassword(charPool);
+  }
+  if (lowercaseCheckBox) {
+    charPool += charset.match(/[a-z]+/g);
+    getPassword(charPool);
+  }
+  if (numberCheckBox) {
+    charPool += String(charset.match(/\d+/g));
+    getPassword(charPool);
+  }
+  if (symbolCheckBox) {
+    charPool += String(charset.match(/\W/g));
+    getPassword(charPool);
+  }
+}
+
+// ========================
+function getPassword(charset) {
   let password = "";
   for (let i = 0; i < inputVol.value; i++) {
     let random = Math.floor(Math.random() * charset.length);
@@ -23,8 +73,6 @@ function generatePassword() {
   randomPassword.innerHTML = password;
   return password;
 }
-
-// ========================
 // ========================
 const footerYear = document.querySelector(".footer-year");
 footerYear.textContent = new Date().getFullYear();
